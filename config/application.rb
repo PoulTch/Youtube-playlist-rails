94e5bc9bf50fa479aea337abfe5337488f80950b
PoulTch <pavelchar@gmail.com>
2025-07-07 14:53:14 +0300

    Disable the Action Cable, as it is not used in the app

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module YoutubePlaylistV1d
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 8.0
 
+    # Отключаем Action Cable, так как он не используется в приложении
+    config.action_cable.mount_path = nil    
+
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
